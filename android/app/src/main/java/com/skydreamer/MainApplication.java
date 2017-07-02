0ae9a9c05ed99261cf9b5e590483330f8ed8ccf9
jkomyno <skiabo97@gmail.com>
2017-07-02 23:51:08 +0200

    Fixed android side adding firebase properly

---
+++ android/app/src/main/java/com/skydreamer/MainApplication.java
@@ -0,0 +1,87 @@
+package com.skydreamer;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.cmcewen.blurview.BlurViewPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.horcrux.svg.SvgPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+// Required package
+import io.invertase.firebase.RNFirebasePackage; // <-- Add this line
+// Optional packages - add as appropriate
+// import io.invertase.firebase.admob.RNFirebaseAdMobPackage; //Firebase AdMob
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage; // Firebase Analytics
+import io.invertase.firebase.auth.RNFirebaseAuthPackage; // Firebase Auth
+// import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage; // Firebase Remote Config
+import io.invertase.firebase.crash.RNFirebaseCrashPackage; // Firebase Crash Reporting
+import io.invertase.firebase.database.RNFirebaseDatabasePackage; // Firebase Realtime Database
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // Firebase Cloud Messaging
+// import io.invertase.firebase.perf.RNFirebasePerformancePackage; // Firebase Messaging
+import io.invertase.firebase.storage.RNFirebaseStoragePackage; // Firebase Storage
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
+            new BlurViewPackage(),
+            new RNFetchBlobPackage(),
+            new SvgPackage(),
+            new VectorIconsPackage(),
+            new LinearGradientPackage(),
+
+              new RNFirebasePackage(),  // <-- Add this line
+              // Add these packages as appropriate
+              // new RNFirebaseAdMobPackage(),
+              new RNFirebaseAnalyticsPackage(),
+              new RNFirebaseAuthPackage(),
+              // new RNFirebaseRemoteConfigPackage(),
+              new RNFirebaseCrashPackage(),
+              new RNFirebaseDatabasePackage(),
+              new RNFirebaseMessagingPackage(),
+              // new RNFirebasePerformancePackage(),
+              new RNFirebaseStoragePackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
